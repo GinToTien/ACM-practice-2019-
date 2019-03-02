2a45ec79dac33deaecd7b7f89fb231a34a80e4b4
aqquan2017 <41032252+aqquan2017@users.noreply.github.com>
2019-03-02 21:49:48 +0700

    Add files via upload

--- Code contest 3/B.cpp
+++ Code contest 3/B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	ios_base::sync_with_stdio(false);
-	short t;
+	int t;
 	cin>>t;
 	
 	while(t--){
@@ -16,12 +16,38 @@ int main(){
 			cin>>b[i];
 		}
 		
-		for(int i=0 ; i<n ; i++){
+		//Selection sort
+		for(int i=0 ; i<n-1 ; i++){
+			int min = i;
+			for(int j=i+1 ; j<n ; j++){
+				if( b[min] > b[j])
+					min = j;
+			}
 			
-		}
-	}		
+			if(min != i){
+				int temp = b[min];
+				b[min] = b[i];
+				b[i] = temp;
+				
+				temp = a[min];
+				a[min] = a[i];
+				a[i] = temp;
+			}	
+		}		
+		//test arr after sort
+//		for(int i=0 ; i<n ; i++){
+//			cout<<a[i]<<" "<<b[i]<<endl;
+//		}
 		
-	
+		int end = b[0], count=1;
+		for(int i=1 ; i<n ; i++){
+			if(end <= a[i]){
+				count++;
+				end = b[i];
+			}
+		}	
+		cout<<count<<endl;
+	}
 	
 	return 0;
 }
